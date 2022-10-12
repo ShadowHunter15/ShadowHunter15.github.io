1aed9ffc7e9920785405c2cb903feb56c492f0cf
Omar Sameh <o.s.dr.who@gmail.com>
2022-10-12 20:57:30 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*============ typing animation  ===== */
+var typed = new Typed(".typing", {
+    strings:["Game Developer","Web Developer","3D Artist","Programmer"],
+    typeSpeed:100,
+    BackSpeed:80,
+    loop:true
+})
+
+var typed = new Typed(".typing1", {
+    strings:["Game Developer","Web Developer","3D Artist","Programmer"],
+    typeSpeed:100,
+    BackSpeed:80,
+    loop:true
+})
